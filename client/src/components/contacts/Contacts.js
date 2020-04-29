dd938bfec8878509ef8309c32f9b5a4e50c72fdb
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-04-28 22:03:40 -0500

    Contacts Context & Global State. Contacts & ContactItem Components

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,22 @@
+import React, { Fragment, useContext } from 'react'
+import ContactItem from './ContactItem'
+import ContactContext from '../../context/contact/contactContext';
+
+const Contacts = () => {
+    
+    const contactContext = useContext(ContactContext);
+
+    const { contacts } = contactContext;
+
+    // eslint-disable-next-line
+
+    return (
+        <Fragment>
+            {contacts.map(contact => (
+                <ContactItem contact={contact} key={contact.id} />
+            ))}
+        </Fragment>
+    )
+}
+
+export default Contacts
