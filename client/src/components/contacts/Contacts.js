7d7a14ab8e029bcdfc75ec3cd225bdbb4afeeeb4
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-04-30 15:35:24 -0500

    basic add & delete animation

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,5 +1,6 @@
-import React, { Fragment, useContext } from 'react'
-import ContactItem from './ContactItem'
+import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group'; 
+import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
@@ -14,9 +15,18 @@ const Contacts = () => {
 
     return (
         <Fragment>
-            {filtered !== null ? filtered.map(contact => (<ContactItem contact={contact} key={contact.id} />)) : contacts.map(contact => (
-                <ContactItem contact={contact} key={contact.id} />
+            <TransitionGroup>
+            {filtered !== null ? filtered.map(contact => (
+                <CSSTransition key={contact.id} timeout={500} classNames="item">
+                    <ContactItem contact={contact} />
+                </CSSTransition>
+                )) 
+            : contacts.map(contact => (
+                <CSSTransition key={contact.id} timeout={500} classNames="item">
+                    <ContactItem contact={contact} />
+                </CSSTransition>
             ))}
+            </TransitionGroup>
         </Fragment>
     )
 }
